4fadc171fb2af14461e04ba48e27008d8323031c
LPDani <v.dan19921119@gmail.com>
2015-09-27 18:24:35 +0200

    map, nodeok

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Node {
+
+    public enum eTileColor
+    {
+        Black = 0,
+        Red = 1,
+        Green = 2
+    };
+
+    eTileColor[] tileColors = new eTileColor[3];
+    public Vector3 nodePosition { get; set; }
+
+    bool isFree = true;
+
+    public Node() { 
+        
+    }
+
+    public Node(Vector3 nodePosition, bool isFree = true) {
+        this.nodePosition = nodePosition;
+        this.isFree = isFree;
+    }
+}
---
+++ Assets/Scripts/NodeManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeManager : MonoBehaviour {
+
+    Node[,] NodeMap = new Node[10,10];
+
+    public int nMapZWidth;
+    public int nMapXLength;
+    public int nMapYHeight;
+
+    public float fNodeSize = 0.6f;
+    public float fNodeHeight = 0.16f;
+
+    public GameObject nodeTexture;
+
+	// Use this for initialization
+	void Start () {
+        float xPos = 0 - nMapXLength / 2 * fNodeSize;
+        float zPos = 0 - nMapZWidth / 2 * fNodeSize;
+        for (int j = 0; j < nMapZWidth; j++ ){
+            for (int i = 0; i < nMapXLength; i++)
+            {
+                NodeMap[i,j] = new Node(new Vector3(xPos + i * fNodeSize, 0, zPos + j * fNodeSize) /* true */);
+               // Debug.Log(NodeMap[i].nodePosition.x + " " + NodeMap[i].nodePosition.y + " " + NodeMap[i].nodePosition.z);
+            }
+            //Debug.Log(j);
+        }
+        foreach (Node node in NodeMap)
+        {
+            Instantiate(nodeTexture, node.nodePosition, new Quaternion(0, 0, 0, 0));
+        }
+    }
+   
+        
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
