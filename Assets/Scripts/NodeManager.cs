a1f0d92ad57cb7debcc17f0132990176054c0d35
LPDani <v.dan19921119@gmail.com>
2015-09-30 11:57:43 +0200

    3d nodegen, UI camera

--- Assets/Scripts/NodeManager.cs
+++ Assets/Scripts/NodeManager.cs
@@ -3,32 +3,42 @@ using System.Collections;
 
 public class NodeManager : MonoBehaviour {
 
-    Node[,] NodeMap = new Node[10,10];
+    //Node[,] NodeMap = new Node[10,10];
 
-    public int nMapZWidth;
-    public int nMapXLength;
-    public int nMapYHeight;
+    public int nMapZWidth = 10;
+    public int nMapXLength = 10;
+    public int nMapYHeight = 3;
+
+    Node[, ,] NodeMap; // = new Node[nMapZWidth, nMapXLength, nMapYHeight];
 
     public float fNodeSize = 0.6f;
     public float fNodeHeight = 0.16f;
 
-    public GameObject nodeTexture;
+    public GameObject nodeObject;
 
 	// Use this for initialization
 	void Start () {
-        float xPos = 0 - nMapXLength / 2 * fNodeSize;
-        float zPos = 0 - nMapZWidth / 2 * fNodeSize;
-        for (int j = 0; j < nMapZWidth; j++ ){
-            for (int i = 0; i < nMapXLength; i++)
+        NodeMap = new Node[nMapZWidth, nMapXLength, nMapYHeight];
+        float xPos = 0.6f - nMapXLength / 2 * fNodeSize;
+        float zPos = 0.6f - nMapZWidth / 2 * fNodeSize;
+        float yPos = 0.16f - nMapYHeight / 2 * fNodeHeight;
+
+        for (int k = 0; k < nMapYHeight; k++) {
+            for (int j = 0; j < nMapZWidth; j++)
             {
-                NodeMap[i,j] = new Node(new Vector3(xPos + i * fNodeSize, 0, zPos + j * fNodeSize) /* true */);
-               // Debug.Log(NodeMap[i].nodePosition.x + " " + NodeMap[i].nodePosition.y + " " + NodeMap[i].nodePosition.z);
+                for (int i = 0; i < nMapXLength; i++)
+                {
+                    NodeMap[i, j, k] = new Node(new Vector3(xPos + i * fNodeSize, yPos + k * fNodeHeight, zPos + j * fNodeSize) /* true */);
+                    // Debug.Log(NodeMap[i].nodePosition.x + " " + NodeMap[i].nodePosition.y + " " + NodeMap[i].nodePosition.z);
+                }
+                //Debug.Log(j);
             }
-            //Debug.Log(j);
         }
         foreach (Node node in NodeMap)
         {
-            Instantiate(nodeTexture, node.nodePosition, new Quaternion(0, 0, 0, 0));
+            //Debug.Log(nodeObject.transform.right);
+            if(node != null)
+                Instantiate(nodeObject, node.nodePosition, Quaternion.Euler( new Vector3(90,0,0) ) );
         }
     }
    
@@ -36,6 +46,6 @@ public class NodeManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
